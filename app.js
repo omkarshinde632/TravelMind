8418c6f864f7b27d6faa0f1015ef579147cf9ca7
omkarshinde632 <152523310+omkarshinde632@users.noreply.github.com>
2026-02-06 23:36:50 +0530

    deployment ready commit

--- app.js
+++ app.js
@@ -16,30 +16,25 @@ app.use(
 
 app.use(expressLayouts);
 app.set("layout", "layout");
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-const savedRoutes = require("./routes/saved.routes");
+
 const searchRoutes = require("./routes/search.routes");
 const tripRoutes = require("./routes/trip.routes");
-const PORT = process.env.PORT || 3000;
+const savedRoutes = require("./routes/saved.routes");
+const compareRoutes = require("./routes/compare.routes");
 
 app.use("/", searchRoutes);
-
-
-app.listen(PORT, () => {
-  console.log(`TravelMind running on http://localhost:${PORT}`);
-});
-
 app.use("/trip", tripRoutes);
-
 app.use("/saved", savedRoutes);
-
-const compareRoutes = require("./routes/compare.routes");
 app.use("/compare", compareRoutes);
 
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(` TravelMind running on port ${PORT}`);
+});
